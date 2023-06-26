100afc19c9f1876dc025a6e8e4394baf3f08b63d
Florian Cebula <ano1995@web.de>
2023-06-26 10:11:30 +0200

    Changed staps (bunkerm powerBunker) layout

--- src/room/blueprints/bunker.ts
+++ src/room/blueprints/bunker.ts
@@ -4,7 +4,7 @@ export const bunkerMatrix: (undefined | bluePrintMatrixElement)[][] = [
 	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 1, type: STRUCTURE_EXTENSION},	{ rcl: 1, type: STRUCTURE_EXTENSION},	undefined,								{ rcl: 1, type: STRUCTURE_EXTENSION},	{ rcl: 1, type: STRUCTURE_EXTENSION},	{ rcl: 1, type: STRUCTURE_ROAD}],
 	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 2, type: STRUCTURE_EXTENSION},	undefined,								{ rcl: 1, type: STRUCTURE_EXTENSION},	undefined,								{ rcl: 2, type: STRUCTURE_EXTENSION},	{ rcl: 1, type: STRUCTURE_ROAD}],
 	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 1, type: STRUCTURE_CONTAINER},	{ rcl: 2, type: STRUCTURE_EXTENSION},	{ rcl: 6, type: STRUCTURE_LINK},		{ rcl: 2, type: STRUCTURE_EXTENSION},	{ rcl: 1, type: STRUCTURE_CONTAINER},	{ rcl: 1, type: STRUCTURE_ROAD}],
-	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 3, type: STRUCTURE_EXTENSION},	undefined,								{ rcl: 2, type: STRUCTURE_EXTENSION},	undefined,								{ rcl: 3, type: STRUCTURE_EXTENSION},	{ rcl: 1, type: STRUCTURE_ROAD}],
-	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 3, type: STRUCTURE_EXTENSION},	{ rcl: 3, type: STRUCTURE_EXTENSION},	{ rcl: 7, type: STRUCTURE_SPAWN},		{ rcl: 3, type: STRUCTURE_EXTENSION},	{ rcl: 3, type: STRUCTURE_EXTENSION},	{ rcl: 1, type: STRUCTURE_ROAD}],
+	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 6, type: STRUCTURE_SPAWN},		undefined,								{ rcl: 2, type: STRUCTURE_EXTENSION},	undefined,								{ rcl: 7, type: STRUCTURE_SPAWN},		{ rcl: 1, type: STRUCTURE_ROAD}],
+	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 3, type: STRUCTURE_EXTENSION},	{ rcl: 3, type: STRUCTURE_EXTENSION},	{ rcl: 3, type: STRUCTURE_EXTENSION},	{ rcl: 3, type: STRUCTURE_EXTENSION},	{ rcl: 3, type: STRUCTURE_EXTENSION},	{ rcl: 1, type: STRUCTURE_ROAD}],
 	[undefined,							{ rcl: 1, type: STRUCTURE_ROAD},		{ rcl: 1, type: STRUCTURE_ROAD},		{ rcl: 1, type: STRUCTURE_ROAD},		{ rcl: 1, type: STRUCTURE_ROAD},		{ rcl: 1, type: STRUCTURE_ROAD},		undefined],
 ]
--- src/room/blueprints/powerBunker.ts
+++ src/room/blueprints/powerBunker.ts
@@ -3,6 +3,6 @@ export const powerBunkerMatrix: (undefined | bluePrintMatrixElement)[][] = [
 	[undefined,							{ rcl: 1, type: STRUCTURE_ROAD},		{ rcl: 1, type: STRUCTURE_ROAD},		{ rcl: 1, type: STRUCTURE_ROAD},		undefined],
 	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 4, type: STRUCTURE_LINK},		{ rcl: 6, type: STRUCTURE_FACTORY},		{ rcl: 3, type: STRUCTURE_STORAGE},		{ rcl: 1, type: STRUCTURE_ROAD}],
 	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 7, type: STRUCTURE_NUKER},		{ rcl: 7, type: STRUCTURE_OBSERVER},	{ rcl: 7, type: STRUCTURE_POWER_SPAWN},	{ rcl: 1, type: STRUCTURE_ROAD}],
-	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 5, type: STRUCTURE_TERMINAL},	{ rcl: 7, type: STRUCTURE_SPAWN},		{ rcl: 1, type: STRUCTURE_ROAD},		undefined],
+	[{ rcl: 1, type: STRUCTURE_ROAD},	{ rcl: 5, type: STRUCTURE_TERMINAL},	undefined,								{ rcl: 1, type: STRUCTURE_ROAD},		undefined],
 	[undefined,							{ rcl: 1, type: STRUCTURE_ROAD},		{ rcl: 1, type: STRUCTURE_ROAD},		undefined,								undefined],
 ]
