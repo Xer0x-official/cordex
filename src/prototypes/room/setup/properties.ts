4f761458639d33f5fcba36c251169cefc3846e61
Florian Cebula <ano1995@web.de>
2025-07-28 19:43:31 +0200

    added active resources and better calculation of active ressources

--- src/prototypes/room/setup/properties.ts
+++ src/prototypes/room/setup/properties.ts
@@ -1,17 +1,17 @@
 
 export function setupProperties(room: Room, remote: boolean, originSpawnPos: RoomPosition) {
-	_.forEach(room.find(FIND_SOURCES), function (source) {
+    if (!remote) {
+        _.forEach(_.filter(Game.spawns, spawn => spawn.room.name === room.name), spawn => room.colonieMemory.spawns.push(spawn.id));
+        room.colonieMemory.myStructurs.push(room.colonieMemory.spawns[0]);
+    }
+
+    _.forEach(room.find(FIND_SOURCES), function (source) {
 		room.colonieMemory.resources.energy[source.id] = initResource(originSpawnPos, source, remote);
 	});
 
 	_.forEach(room.find(FIND_MINERALS), function (source) {
 		room.colonieMemory.resources.minerals[source.id] = initResource(originSpawnPos, source, remote);
 	});
-
-	if (!remote) {
-		_.forEach(_.filter(Game.spawns, spawn => spawn.room.name === room.name), spawn => room.colonieMemory.spawns.push(spawn.id));
-		room.colonieMemory.myStructurs.push(room.colonieMemory.spawns[0]);
-	}
 }
 
 function initResource(originSpawnPos: RoomPosition, source: Source | Mineral, remote: boolean): ISourceMemory {
